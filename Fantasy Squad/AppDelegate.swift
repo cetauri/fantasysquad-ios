a5dad3950fed4e1be7e39fd465fa51f48e9d484a
Kwon, oh-sang <cetauri@gmail.com>
2017-05-22 18:57:43 +0200

    git init

--- Fantasy Squad/AppDelegate.swift
+++ Fantasy Squad/AppDelegate.swift
@@ -16,6 +16,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // iOS 10 support
+//        if #available(iOS 10, *) {
+//            let center = UNUserNotificationCenter.current()
+//            center.requestAuthorization(options:[.badge, .alert, .sound]){ (granted, error) in }
+//            application.registerForRemoteNotifications()
+//        }
+//            // iOS 9 support
+//        else if #available(iOS 9, *) {
+            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+            UIApplication.shared.registerForRemoteNotifications()
+//        }
+            // iOS 8 support
+         if #available(iOS 8, *) {
+            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+            // iOS 7 support
+        else {  
+            application.registerForRemoteNotifications(matching: [.badge, .sound, .alert])
+        }
+        
+        print("finish")
         return true
     }
 
@@ -41,6 +64,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    
+    
+    // Called when APNs has assigned the device a unique token
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // Convert token to string
+        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        
+        // Print it to console
+        print("APNs device token: \(deviceTokenString)")
+        
+        // Persist it in your backend in case it's new
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "GetTokenNotification"), object: nil, userInfo: ["key" : "\(deviceTokenString)"])
 
+    }
+    
+    // Called when APNs failed to register the device for push notifications
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        // Print the error to console (you should alert the user that registration failed)
+        print("APNs registration failed: \(error)")
+    }
+    
+    
+    // Push notification received
+    func application(_ application: UIApplication, didReceiveRemoteNotification data: [AnyHashable : Any]) {
+        // Print notification payload data
+        print("Push notification received: \(data)")
+        
+        // notification
+//        self.window?.rootViewController?.f
+        
+    }
 }
 
--- Fantasy Squad/FirstViewController.swift
+++ Fantasy Squad/FirstViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(getTokenNotification),
+                                               name: NSNotification.Name(rawValue: "GetTokenNotification"),
+                                               object: nil)
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func getTokenNotification(_ notification: Notification) {
+        textField.text = notification.userInfo?["key"] as? String
+        
+    }
 }
 
